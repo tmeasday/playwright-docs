38c20ec7328bb296df0455f505f225a88d03ccfd
Steven Kitterman <steven.kitterman@gmail.com>
2024-02-06 12:32:49 -0800

    asdf

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,6 +8,13 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('has something', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
 test('get started link', async ({ page }, testInfo) => {
   await page.goto('https://playwright.dev/');
 
