78d6abd28c8ad279c6365e52e2e5eb1dfca5decc
Steven Kitterman <steven.kitterman@gmail.com>
2024-02-06 12:28:30 -0800

    asdf

---
+++ tests/example.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+const { test, expect, takeSnapshot } = require('chromatic-playwright');
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }, testInfo) => {
+  await page.goto('https://playwright.dev/');
+
+  await takeSnapshot(page, testInfo);
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
